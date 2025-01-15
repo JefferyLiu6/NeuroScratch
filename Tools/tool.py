24987ec600d5dbbf49cbb901424463ce31a999d8
“JefferyLiu6” <jefferyliu6@gmail.com>
2025-01-15 14:29:23 -0500

    loss

---
+++ Tools/tool.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+def mse_loss(y, y_pred):
+
+def d_mse_loss(y, y_pred):
+
+def mae_loss(y, y_pred):
+
+def d_mae_loss(y, y_pred):
+
+def cross_entropy_loss(y, y_pred):
+
+def d_cross_entropy_loss(y, y_pred):
+
+#
+#
